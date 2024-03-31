aa4db7d7921e19d27927d6a70cece2e58d9238f5
dor.frechter <dor.frechter@gmail.com>
2024-03-30 22:37:30 -0700

    d

--- app.py
+++ app.py
@@ -4,14 +4,21 @@ from dash import Dash, html, dcc
 from dash.dependencies import Input, Output  # Make sure to import Input and Output
 import pandas as pd
 import plotly.express as px
-barca_squad_df = pd.read_csv('/Users/dor.frechter/Desktop/DS4003/Final Project/data/data.csv')
-barca_squad_df.drop(columns=['shirt_number','is_overseas','birthdate','team'], axis=1,inplace=True)
-barca_squad_df['country'] = barca_squad_df['country'].astype('string')
-barca_squad_df['position'] = barca_squad_df['position'].astype('string')
-barca_squad_df['role'] = barca_squad_df['role'].astype('string')
-barca_squad_df['full_name'] = barca_squad_df['full_name'].astype('string')
-barca_squad_df['season'] = barca_squad_df['season'].astype('string')
-barca_squad_df.head()
+messi_goals_df = pd.read_csv('data1.csv')
+messi_goals_df.drop('Playing_Position', axis=1, inplace=True)
+messi_goals_df.drop('Matchday', axis=1, inplace=True)
+messi_goals_df.drop('Date', axis=1, inplace=True)
+messi_goals_df.drop('Venue', axis=1, inplace=True)
+messi_goals_df.drop('At_score', axis=1, inplace=True)
+messi_goals_df.drop('Result', axis=1, inplace=True)
+messi_goals_df['Season'] = messi_goals_df['Season'].astype('string')
+messi_goals_df['Competition'] = messi_goals_df['Competition'].astype('string')
+messi_goals_df['Club'] = messi_goals_df['Club'].astype('string')
+messi_goals_df['Type'] = messi_goals_df['Type'].astype('string')
+messi_goals_df['Goal_assist'] = messi_goals_df['Goal_assist'].astype('string')
+messi_goals_df['Opponent'] = messi_goals_df['Opponent'].astype('string')
+messi_goals_df['Minute'] = messi_goals_df['Minute'].astype('string')
+messi_goals_df.head()
 
 #get dataset
 df = pd.read_csv('gdp_pcap.csv')
